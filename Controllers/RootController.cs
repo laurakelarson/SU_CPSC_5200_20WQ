7a5e77494a3fdc41b050fd3daf6692a67354dad6
Laura Larson <31149925+laurakelarson@users.noreply.github.com>
2020-01-23 14:57:47 -0800

    Add files via upload

---
+++ Controllers/RootController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using restapi.Models;
+using Microsoft.Extensions.Logging;
+
+namespace restapi.Controllers
+{
+    public class RootController : Controller
+    {
+        private readonly TimesheetsRepository repository;
+        private readonly ILogger logger;
+
+        public RootController(ILogger<RootController> l){
+            repository = new TimesheetsRepository();
+            logger = l;
+        }
+
+        // GET api/values
+        [Route("~/")]
+        [HttpGet]
+        [Produces(ContentTypes.Root)]
+        [ProducesResponseType(typeof(IDictionary<ApplicationRelationship, object>), 200)]
+        public IDictionary<ApplicationRelationship, object> Get()
+        {
+            return new Dictionary<ApplicationRelationship, object>()
+            {
+                {
+                    ApplicationRelationship.Timesheets, new List<DocumentLink>()
+                    {
+                        new DocumentLink()
+                        {
+                            Method = Method.Get,
+                            Type = ContentTypes.Timesheets,
+                            Relationship = DocumentRelationship.Timesheets,
+                            Reference = "/timesheets"
+                        }
+                    }
+                },
+                {
+                    ApplicationRelationship.Version, "0.1"
+                }
+            };
+        }
+
+        // Create new timecard object
+        [Route("~/")]
+        [HttpPost]
+        [Produces(ContentTypes.Timesheet)]
+        [ProducesResponseType(typeof(Timecard), 200)]
+        public Timecard Create([FromBody] DocumentPerson p){
+            logger.LogInformation($"Creation of timesheet for {p.ToString()}");
+            var tc = new Timecard(p.Id);
+            var entered = new Entered() {Person = p.Id};
+            tc.Transitions.Add(new Transition(entered));
+            repository.Add(tc);
+            return tc;
+        }
+    }
+}
---
+++ Models/ErrorMessages.cs
@@ -0,0 +1,29 @@
+namespace restapi.Models
+{
+    public class InvalidStateError
+    {
+        public int ErrorCode { get => 100; }
+
+        public string Message { get => "Transition not valid for current state"; }
+    }
+
+    public class EmptyTimecardError
+    {
+        public int ErrorCode { get => 101; }
+
+        public string Message { get => "Unable to submit timecard with no lines"; }
+    }
+
+    public class MissingTransitionError
+    {
+        public int ErrorCode { get => 102; }
+
+        public string Message { get => "No state transition of requested type present in timecard"; }
+    }
+
+    public class UnauthorizedApprovalError {
+        public int ErrorCode { get => 103; }
+
+        public string Message { get => "Approver of the timesheet cannot be the employee on timesheet."; }
+    }
+}
